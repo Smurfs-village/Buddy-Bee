35660f9dbea8a5a8290929865741d8b6f4975081
yoonlix <yoooon0718@naver.com>
2024-07-05 13:17:06 +0900

    login page ui compelete

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout/Layout";
 import BackGroundGrid from "../../components/Layout/BackGroundGrid";
 import backgroundImg from "../../img/login_illust.svg";
 import frontCloudImg from "../../img/login_cloud.svg";
+import loginIcon from "../../img/login_bee.svg";
 
 const LoginPage = () => {
   return (
@@ -14,13 +15,26 @@ const LoginPage = () => {
           </div>
           <div className="login-page-wrapper">
             <div className="login-page-action-bar">
-              <div className="three-color">
-                <div className="three-color-circle three-green"></div>
-                <div className="three-color-circle three-yellow"></div>
-                <div className="three-color-circle three-red"></div>
+              <div className="login-page-three-color">
+                <div className="login-page-color-circle color-green"></div>
+                <div className="login-page-color-circle color-yellow"></div>
+                <div className="login-page-color-circle color-red"></div>
               </div>
             </div>
-            <div className="login-icon"></div>
+            <div className="login-page-content-wrapper">
+              <div className="login-page-icon-wrapper">
+                <img src={loginIcon} alt="loginIcon" />
+                <h2>LOGIN</h2>
+              </div>
+              <div className="login-page-input-wrapper">
+                <input type="email" placeholder="이메일 주소" required />
+                <input type="password" placeholder="비밀번호" required />
+              </div>
+            </div>
+            <div className="login-page-btn-wrapper">
+              <button className="login-page-sign-up-btn">Sign up</button>
+              <button className="login-page-login-btn">Login</button>
+            </div>
           </div>
           <div className="login-page-cloud-img">
             <img src={frontCloudImg} alt="cloudImg" />

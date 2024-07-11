71dc6e19dd932fdd62eefda1b51232459ee95ceb
Shugaroon <rnballard@naver.com>
2024-07-11 17:27:17 +0900

    myPage need help

--- frontend/src/views/ProfilePage/Common.js
+++ frontend/src/views/ProfilePage/Common.js
@@ -1,4 +1,7 @@
+import "./Common.css";
 import myPageflowerImg from "../../img/myPage_flower.svg";
+import Paging from "react-js-pagination";
+import rightArrow from "../../img/right_arrow.svg";
 
 export const FlowerImg = () => {
     return (
@@ -9,3 +12,33 @@ export const FlowerImg = () => {
         />
     );
 };
+
+const Pagination = ({
+    totalItemsCount,
+    handlePageChange,
+    activePage,
+    itemsCountPerPage,
+    pageRangeDisplayed,
+}) => {
+    return (
+        <Paging
+            activePage={activePage}
+            itemsCountPerPage={itemsCountPerPage}
+            totalItemsCount={totalItemsCount}
+            pageRangeDisplayed={pageRangeDisplayed}
+            prevPageText={null}
+            nextPageText={
+                <img
+                    className="pagination-right-arrow"
+                    src={rightArrow}
+                    alt="next"
+                />
+            }
+            onChange={handlePageChange}
+            firstPageText={null}
+            lastPageText={null}
+        />
+    );
+};
+
+export default Pagination;

14c87aba05960737a6b4bf55c2f1b96a894a25d3
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-07-12 11:11:37 +0900

    react-out

--- frontend/src/components/Common/Top.js
+++ frontend/src/components/Common/Top.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import cloudImg from "../../img/cloud2.svg";
 import "./Top.css";

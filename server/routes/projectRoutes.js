443ad141035cd829001dac7bffec846be3525217
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-07-15 18:29:41 +0900

    feat : profile-honeypot-onoff-back-add #256

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -3,28 +3,32 @@ const projectController = require("../controllers/projectController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/", projectController.createProject);
-router.get("/", projectController.getProjects);
-router.get("/user", authMiddleware, projectController.getUserProjects);
-router.get("/search", projectController.searchProjects);
+router.post("/", authMiddleware, projectController.createProject); // 인증 필요
+router.get("/", projectController.getProjects); // 인증 불필요
+router.get("/user", authMiddleware, projectController.getUserProjects); // 인증 필요
+router.get("/search", projectController.searchProjects); // 인증 불필요
 router.get(
   "/participated",
   authMiddleware,
   projectController.getParticipatedProjects
-);
+); // 인증 필요
 router.get(
   "/bookmarked",
   authMiddleware,
   projectController.getBookmarkedProjects
-);
-router.get("/:id", projectController.getProjectById);
-router.get("/:id/hashtags", projectController.getProjectHashtags);
-router.get("/:id/participants", projectController.getProjectParticipants);
-router.get("/:id/honey", projectController.getProjectHoney);
-router.post("/:id/honey", projectController.addProjectHoney);
-router.delete("/:id/honey", projectController.removeProjectHoney);
-router.patch("/:id/increment-view", projectController.incrementViewCount);
-router.post("/:id/toggle-honey", projectController.toggleHoney);
-router.get("/:id/honey/:userId", projectController.checkProjectHoney);
+); // 인증 필요
+router.get("/:id", projectController.getProjectById); // 인증 불필요
+router.get("/:id/hashtags", projectController.getProjectHashtags); // 인증 불필요
+router.get("/:id/participants", projectController.getProjectParticipants); // 인증 불필요
+router.get("/:id/honey", projectController.getProjectHoney); // 인증 불필요
+router.post("/:id/honey", authMiddleware, projectController.addProjectHoney); // 인증 필요
+router.delete(
+  "/:id/honey",
+  authMiddleware,
+  projectController.removeProjectHoney
+); // 인증 필요
+router.patch("/:id/increment-view", projectController.incrementViewCount); // 인증 불필요
+router.post("/:id/toggle-honey", authMiddleware, projectController.toggleHoney); // 인증 필요
+router.get("/:id/honey/:userId", projectController.checkProjectHoney); // 인증 불필요
 
 module.exports = router;

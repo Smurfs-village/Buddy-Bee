f9e799877286e3cbd5d0fe8c83f33cd94e6274c4
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-07-18 20:27:19 +0900

    feat : 메인페이지 모바일 카드 사이즈 수정 #316

--- frontend/src/views/MainPage/MainPage.js
+++ frontend/src/views/MainPage/MainPage.js
@@ -19,12 +19,35 @@ const MainPage = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     autoplaySpeed: 3000,
     arrows: true,
     draggable: true,
     pauseOnHover: true,
     variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const [recruitmentCards, setRecruitmentCards] = useState([]);

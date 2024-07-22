f1bf03bc627f236a6ce4c056f4390413fac5a499
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-07-22 20:39:56 +0900

    feat : 도커 추가

--- server/models/db.js
+++ server/models/db.js
@@ -3,20 +3,36 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connectionConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   multipleStatements: true,
-});
+};
 
-connection.connect(err => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL");
-});
+const connectWithRetry = () => {
+  const connection = mysql.createConnection(connectionConfig);
 
-module.exports = connection;
+  connection.connect(err => {
+    if (err) {
+      console.error("Error connecting to MySQL, retrying in 5 seconds...", err);
+      setTimeout(connectWithRetry, 5000);
+    } else {
+      console.log("Connected to MySQL");
+    }
+  });
+
+  connection.on("error", function (err) {
+    console.error("MySQL error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      connectWithRetry();
+    } else {
+      throw err;
+    }
+  });
+
+  module.exports = connection;
+};
+
+connectWithRetry();
